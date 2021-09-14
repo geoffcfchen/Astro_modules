a4824e84bd7605f113ba402ad34cf5ccf4cd66b3
chinfanster <chinfanster@gmail.com>
2021-09-14 11:12:30 +0800

    fix bug

--- resample_preserveflux.py
+++ resample_preserveflux.py
@@ -22,18 +22,20 @@ def resample_1D_prevflux(array,pixel_size,new_pixel_size):
     :param new_pixel_size: the new pixel size for the output 1D array
     :return: new 1D array which preserve the flux, and the corresponding matrix
     """
-    array=array#np.array([0,1,2]) ## example chains
+    array=array#np.arange(31)#array#np.array([0,1,2]) ## example chains
     N = array.shape[0]
+    array=np.append(array,0.)
     pixel_size= pixel_size #10.  ##old pixel size
-    axis_dis=np.linspace(0,pixel_size*N,N+1)  #create the locations for the
-    # pixel boundary in old array
+    axis_dis=np.linspace(0,pixel_size*(N+1),N+2)  #create the locations for the
+    # pixel boundary in old array and create one extra pixel
     new_pixel_size= new_pixel_size #6.  # new pixel size
     total_length=pixel_size*N
-    new_axis_dis=np.linspace(0.,total_length,
-                             int(pixel_size*N/new_pixel_size+1)) #create the
+    N_new = int(pixel_size * N / new_pixel_size)+2
+    new_axis_dis = np.arange(N_new)*new_pixel_size #create the
     #  locations for the pixel boundary in new array
-    new_array=np.zeros(int(pixel_size*N/new_pixel_size))
-    matrix=np.zeros((int(pixel_size*N/new_pixel_size),N))
+    new_array=np.zeros(N_new-1)
+    matrix=np.zeros((new_array.shape[0],N+1))
+    #print(matrix.shape)
     for i in range(new_array.shape[0]):
         # this is to identify if new pixel is located completely inside the
         # old pixel
@@ -41,8 +43,10 @@ def resample_1D_prevflux(array,pixel_size,new_pixel_size):
                 np.sort(np.argsort(np.abs(axis_dis-new_axis_dis[i+1]))[:2])):
             ind_in_oldarray=np.sort(np.argsort(np.abs(axis_dis-new_axis_dis[
                 i]))[:1])[0]
+            #print(ind_in_oldarray)
             new_array[i] = new_pixel_size / pixel_size * array[ind_in_oldarray]
             matrix[i,ind_in_oldarray]= new_pixel_size / pixel_size
+            #print("locate inside",i)
         # this is to identify if new pixel is located partially inside the
         # new pixel
         elif all(np.sort(np.argsort(np.abs(axis_dis-new_axis_dis[i]))[:2]) !=\
@@ -51,6 +55,7 @@ def resample_1D_prevflux(array,pixel_size,new_pixel_size):
                 axis_dis-new_axis_dis[i]))[:2])[0]
             ind_in_oldarray_2=np.sort(np.argsort(np.abs(
                 axis_dis-new_axis_dis[i+1]))[:2])[0]
+            #print(ind_in_oldarray_1,ind_in_oldarray_2)
             old_array_value_center=axis_dis[ind_in_oldarray_1+1]
             ratio1=abs((new_axis_dis[i]-old_array_value_center)/pixel_size)
             ratio2 = abs(
@@ -59,8 +64,14 @@ def resample_1D_prevflux(array,pixel_size,new_pixel_size):
                 ind_in_oldarray_2]
             matrix[i,ind_in_oldarray_1]=ratio1
             matrix[i,ind_in_oldarray_2]=ratio2
+            #print("locate parial", i)
         else:
             print("something wrong")
+    matrix=np.delete(matrix, -1, axis=1) ##remove last column
     return new_array, matrix
 
 
+##for testing if it is flux preserved
+#new_y,matrix=resample_1D_prevflux(y_axis,1,0.44)
+#np.sum(y_axis)
+#np.sum(new_y)
